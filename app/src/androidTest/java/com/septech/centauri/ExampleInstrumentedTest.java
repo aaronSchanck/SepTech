a040776ae660cf73333b3de233f61398a61cdd8a
ItsmeCurly <adamg765@gmail.com>
2021-02-25 12:58:19 -0500

    update project to reflect centauri naming style, update android api to marshmallow (23)

--- app/src/androidTest/java/com/septech/centauri/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/septech/centauri/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.septech", appContext.getPackageName());
+        assertEquals("com.septech.centauri", appContext.getPackageName());
     }
 }
