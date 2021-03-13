3d7aaa59738db3d5333c40c1026043342aa34543
ItsmeCurly <adamg765@gmail.com>
2021-03-13 16:52:47 -0500

    attempted fix for the logging issue

--- web/utils/logging.py
+++ web/utils/logging.py
@@ -8,47 +8,36 @@ class DebugFilter(logging.Filter):
         return rec.levelno == logging.DEBUG
 
 
-@contextlib.contextmanager
 def setup_logging():
-    try:
-        # __enter__
-        max_bytes = 32 * 1024 * 1024  # 32 MiB
-        logging.getLogger("centauri").setLevel(logging.INFO)
-        logging.getLogger("centauri-debug").setLevel(logging.DEBUG)
+    max_bytes = 32 * 1024 * 1024  # 32 MiB
+    logging.getLogger("centauri").setLevel(logging.INFO)
+    logging.getLogger("centauri-debug").setLevel(logging.DEBUG)
 
-        log = logging.getLogger()
-        log.setLevel(logging.DEBUG)
-        handler = RotatingFileHandler(
-            filename="centauri.log",
-            encoding="utf-8",
-            mode="w",
-            maxBytes=max_bytes,
-            backupCount=5,
-        )
+    log = logging.getLogger()
+    log.setLevel(logging.DEBUG)
+    handler = RotatingFileHandler(
+        filename="centauri.log",
+        encoding="utf-8",
+        mode="w",
+        maxBytes=max_bytes,
+        backupCount=5,
+    )
 
-        handler2 = RotatingFileHandler(
-            filename="centauri-debug.log",
-            encoding="utf-8",
-            mode="w",
-            maxBytes=max_bytes,
-            backupCount=5,
-        )
-        dt_fmt = "%Y-%m-%d %H:%M:%S"
-        fmt = logging.Formatter(
-            "[{asctime}] [{levelname:<7}] {name}: {message}", dt_fmt, style="{"
-        )
-        handler.setFormatter(fmt)
-        handler2.setFormatter(fmt)
+    handler2 = RotatingFileHandler(
+        filename="centauri-debug.log",
+        encoding="utf-8",
+        mode="w",
+        maxBytes=max_bytes,
+        backupCount=5,
+    )
+    dt_fmt = "%Y-%m-%d %H:%M:%S"
+    fmt = logging.Formatter(
+        "[{asctime}] [{levelname:<7}] {name}: {message}", dt_fmt, style="{"
+    )
+    handler.setFormatter(fmt)
+    handler2.setFormatter(fmt)
 
-        handler2.addFilter(DebugFilter())
+    handler2.addFilter(DebugFilter())
 
-        log.addHandler(handler)
-        log.addHandler(handler2)
-
-        yield
-    finally:
-        # __exit__
-        handlers = log.handlers[:]
-        for hdlr in handlers:
-            hdlr.close()
-            log.removeHandler(hdlr)
+    log.addHandler(handler)
+    log.addHandler(handler2)
--- web/wsgi.py
+++ web/wsgi.py
@@ -21,14 +21,13 @@ from app import create_app
 
 from utils.logging import setup_logging
 
-log = logging.getLogger(__name__)
-
 env = os.getenv("FLASK_ENV") or "test"
 
 app = create_app(env)
 
+setup_logging()
+
 host = "localhost" if env == "test" else "0.0.0.0"
 
 if __name__ == "__main__":
-    with setup_logging():
-        app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
