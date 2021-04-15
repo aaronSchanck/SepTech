566d9348219a0b13e8ac1279d20873d7c6f220a1
ItsmeCurly <adamg765@gmail.com>
2021-04-15 17:57:14 -0400

    adding more functionality to search

--- app/src/main/java/com/septech/centauri/ui/user/home/HomeViewModel.java
+++ app/src/main/java/com/septech/centauri/ui/user/home/HomeViewModel.java
@@ -1,17 +1,54 @@
 package com.septech.centauri.ui.user.home;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.septech.centauri.data.repository.UserRepositoryImpl;
+import com.septech.centauri.domain.models.User;
 import com.septech.centauri.domain.repository.UserRepository;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class HomeViewModel extends ViewModel {
     private UserRepository userRepo;
+
+    private final CompositeDisposable mDisposables;
+
+    private MutableLiveData<User> userLiveData;
 
     private int userId;
 
     public HomeViewModel() {
         userRepo = UserRepositoryImpl.getInstance();
+
+        mDisposables = new CompositeDisposable();
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        mDisposables.clear();
+    }
+
+    private void getUser() {
+        mDisposables.add(userRepo.getUserById(userId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<User>() {
+                    @Override
+                    public void onSuccess(@NonNull User user) {
+                        System.out.println("user = " + user);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        System.out.println("e = " + e);
+                    }
+                }));
     }
 
     public int getUserId() {
@@ -20,5 +57,13 @@ public class HomeViewModel extends ViewModel {
 
     public void setUserId(int userId) {
         this.userId = userId;
+    }
+
+    public MutableLiveData<User> getUserLiveData() {
+        if(userLiveData == null) {
+            userLiveData = new MutableLiveData<>();
+            getUser();
+        }
+        return userLiveData;
     }
 }
