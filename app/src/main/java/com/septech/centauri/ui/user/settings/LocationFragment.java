cde0a3e517996fcb971846a0e22bf23a35614be4
Aaron Schanck <aaron.schanck@maine.edu>
2021-04-18 20:51:57 -0400

    push settings, location page fragment

---
+++ app/src/main/java/com/septech/centauri/ui/user/settings/LocationFragment.java
@@ -0,0 +1,38 @@
+package com.septech.centauri.ui.user.settings;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.septech.centauri.R;
+
+public class LocationFragment extends Fragment {
+
+    private LocationViewModel mViewModel;
+
+    public static LocationFragment newInstance() {
+        return new LocationFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.user_location_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(LocationViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
