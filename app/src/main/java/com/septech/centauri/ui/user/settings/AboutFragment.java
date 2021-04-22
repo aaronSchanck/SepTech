c73ca61bfddb0e44b5ba9cc5cdf5d80108a32326
Aaron Schanck <aaron.schanck@maine.edu>
2021-04-22 12:45:10 -0400

    push settings/about page fragment

---
+++ app/src/main/java/com/septech/centauri/ui/user/settings/AboutFragment.java
@@ -0,0 +1,38 @@
+package com.septech.centauri.ui.user.settings;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.septech.centauri.R;
+
+public class AboutFragment extends Fragment {
+
+    private AboutViewModel mViewModel;
+
+    public static AboutFragment newInstance() {
+        return new AboutFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.user_about_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(AboutViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
